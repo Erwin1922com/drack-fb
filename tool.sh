742f0ea1c26cc9ec0753566c25af3a74267585fa
Erwin <76240546+erwin1922@users.noreply.github.com>
2020-12-19 22:01:46 +0700

    Add files via upload

---
+++ tool.sh
@@ -0,0 +1,12 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+#TOOL Drack Fb
+#LANGUAGE. Bash
+#Author: ErwinSH
+
+echo "[~] Selamet Drack Fb [~]"
+echo "[~] Gunakan Dengan Bijak [~]"
+echo "[~] BY : Erwin Riweh [~]"
+
+read -f "Masukkan Id Publik Temen :" Pilih
+read -f
