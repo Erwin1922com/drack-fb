51bb50bbec6e0eaa70ae936e31b51d06ec2cfbd4
Erwin <76240546+erwin1922@users.noreply.github.com>
2020-12-22 08:15:33 +0700

    Add files via upload

---
+++ drack-fb.py
@@ -0,0 +1,50 @@
+#!usr/bin/bash
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+echo
+echo" selamat datang di tolls aa erwin"
+echo $me"─────────────────
+echo $cy" AUTHOR :MR. ERWIN 1922"
+echo $cy" RIWEH :TEAM. ELLITE"
+echo $cy" whatsapp :0895328288088"
+echo $me"─────────────────
+echo
+echo $pr"SILAHKAN PILIH MENU"
+echo $pu"(1)darkfb"
+echo $pu"(2)darkfb"
+echo $pu"(3)darkfb"
+read -p " [PSHT] : pilih : " pil :
+#batas gan
+if [ $pil = 1 ]
+then
+git clone https://github.com/Erwin1922com/drack-fb
+ls
+cd drack-fb
+ls
+sh drack-fb
+fi
+#batas gan
+if [ $pil = 2 ]
+then
+git clone https://github.com/Erwin1922com/drack-fb
+ls
+cd drack-fb
+ls
+sh drack-fb
+fi
+#batas gan
+if [ $pil = 3 ]
+then
+git clone https://github.com/Erwin1922com/drack-fb
+ls
+ce drack-fb
+ls
+sh drack-fb
+fi
